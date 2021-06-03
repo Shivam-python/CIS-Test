93f6f9d52b2e2478c2d072d2fda42cb095c05ca5
gst-shivam <shivam@genuinesofttechnologies.com>
2021-06-03 12:18:54 +0530

    creds removed

--- testing/settings.py
+++ testing/settings.py
@@ -172,5 +172,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'threat.xpert@gmail.com'
-EMAIL_HOST_PASSWORD = 'threat12345'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
